4571a42bc0bf1af125f77cbb090496f7455ff7ba
Monica C <tigina18@gmail.com>
2020-11-28 21:01:52 -0600

    Steps 1 complete

--- TechJobsOO/PositionType.cs
+++ TechJobsOO/PositionType.cs
@@ -19,5 +19,22 @@ namespace TechJobsOO
         }
 
         // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
+
+        public override bool Equals(object obj)
+        {
+            return obj is PositionType type &&
+                   Id == type.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
     }
 }
